adcadb33e6be0ff486fbe6e43cd4b9cbb2d46d4c
Jeremy Huot <logflume@live.com>
2020-05-16 21:46:01 +0000

    changed for loop to while loop in packing list

--- packing-list.js
+++ packing-list.js
@@ -5,3 +5,10 @@ console.log("Kitchen stuff to pack");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+let i = 0;
+
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
